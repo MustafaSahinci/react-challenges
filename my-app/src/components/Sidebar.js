5f8455472edd0dd1ddbac6449fd2bccc9f5f3ade
Mustafa <mustafa_sahinci@hotmail.com>
2022-10-03 11:18:30 +0000

    first commit

---
+++ my-app/src/components/Sidebar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import css from "./css/Sidebar.module.css";
+
+function SideBar(){
+    return (
+        <div className={css.sidebar}>
+            <a href = "#." target= "_blank">My Photos</a>
+            <a href = "#." target= "_blank">My Illustrations</a>
+            <a href = "#." target= "_blank">My Paintings</a>
+        </div>
+    )
+}
+
+// const SideBar = () =>{
+//     return (
+//         <div>
+//             <a href = "#." target= "_blank">My Photos</a>
+//             <a href = "#." target= "_blank">My Illustrations</a>
+//             <a href = "#." target= "_blank">My Paintings</a>
+//         </div>
+//     )
+// }
+
+export default SideBar
