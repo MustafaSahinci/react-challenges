a19ebf6d7a013a29e70382abdc76bf34bb8573a7
Mustafa <mustafa_sahinci@hotmail.com>
2022-10-10 12:55:12 +0000

    add search form

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -9,26 +9,46 @@ export class Content extends Component {
     super(props)
 
     this.state = {
-      isLoaded: false
+      isLoaded: false,
+      posts: []
     }
   }
   componentDidMount() {
     setTimeout(()=>{
         this.setState({
             isLoaded: true,
+            posts: savedPosts
         })
     }, 2000)
+  }
+  HandleInput = (event) => {
+    const name = event.target.value.toLowerCase()
+    console.log(name)
+    const filteredPosts = savedPosts.filter(posts => {
+      return posts.name.toLowerCase().includes(name)
+    })
+    this.setState({
+      posts: filteredPosts
+    })
   }
   render() {
     return (
       <div className= {css.Content}>
         <div className= {css.TitleBar}>
             <h1>My Photos</h1>
+            <form>
+              <label htmlFor='searchInput'>Search</label>
+              <input onChange={(event) => this.HandleInput(event)}
+                     type="search" 
+                     id= "searchInput" 
+                     placeholder="By Author" />
+              <h4>posts found:{this.state.posts.length}</h4>
+            </form>
         </div>
         <div className= {css.SearchResults}>
           {
             this.state.isLoaded ?
-            <PostItem savedPosts={savedPosts} />
+            <PostItem savedPosts={this.state.posts} />
             : <Loader />
           }
         </div>
