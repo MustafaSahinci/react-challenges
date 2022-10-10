5cea39debf37731e72a4da1286fef6111495928b
Mustafa <mustafa_sahinci@hotmail.com>
2022-10-10 08:15:50 +0000

    learning react

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -2,10 +2,22 @@ import React, { Component } from 'react';
 import {savedPosts} from "../posts.json";
 import css from "./css/Content.module.css";
 import PostItem from './ PostItem';
+import Loader from './Loader';
 
 export class Content extends Component {
   constructor(props) {
     super(props)
+
+    this.state = {
+      isLoaded: false
+    }
+  }
+  componentDidMount() {
+    setTimeout(()=>{
+        this.setState({
+            isLoaded: true,
+        })
+    }, 2000)
   }
   render() {
     return (
@@ -14,7 +26,11 @@ export class Content extends Component {
             <h1>My Photos</h1>
         </div>
         <div className= {css.SearchResults}>
+          {
+            this.state.isLoaded ?
             <PostItem savedPosts={savedPosts} />
+            : <Loader />
+          }
         </div>
         {/* <div>
         {savedPosts.map(post => {
